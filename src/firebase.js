d3597effd12e94c0767de4d37213b08cbab34039
Mark Fortes <mlgmokus@gmail.com>
2024-10-02 15:41:17 -0500

    changed app.jsx to site title

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAQ8jou2G5sbZJkixID8zAojxUhqrLeXCo",
+  authDomain: "cs392-react-tutorial-f1c54.firebaseapp.com",
+  projectId: "cs392-react-tutorial-f1c54",
+  storageBucket: "cs392-react-tutorial-f1c54.appspot.com",
+  messagingSenderId: "283241257120",
+  appId: "1:283241257120:web:e48cf12d0a5102c90dd1c5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
